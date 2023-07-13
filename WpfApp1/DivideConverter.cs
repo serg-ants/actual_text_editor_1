9cef32a3bf851b192afb090fde898a9264193c85
serg-ants <serg.antsipovich@yandex.by>
2023-07-13 20:11:47 +0300

    Initial commit

---
+++ WpfApp1/DivideConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Globalization;
+using System.Windows.Data;
+
+
+namespace WpfApp1
+{
+
+    public class DivideConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double doubleValue = System.Convert.ToDouble(value);
+            double doubleParameter = System.Convert.ToDouble(parameter);
+
+            return doubleValue / doubleParameter;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+}
---
+++ WpfApp1/MultiplyConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WpfApp1
+{
+    public class MultiplyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || parameter == null)
+                return value;
+
+            double doubleValue;
+            double doubleParameter;
+
+            if (double.TryParse(value.ToString(), out doubleValue) && double.TryParse(parameter.ToString(), out doubleParameter))
+                return doubleValue * doubleParameter;
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
